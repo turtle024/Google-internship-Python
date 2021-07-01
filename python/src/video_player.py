9ef9482be95430870629044cd41b6f5518638898
turtle024 <86776529+turtle024@users.noreply.github.com>
2021-07-01 11:18:58 +0100

    Add files via upload

---
+++ python/src/video_player.py
@@ -0,0 +1,222 @@
+"""A video player class."""
+import random
+
+from .video_library import VideoLibrary
+
+
+class VideoPlayer:
+    """A class used to represent a Video Player."""
+
+    def __init__(self):
+        self._video_library = VideoLibrary()
+        self._current_video = None
+        self._pause_video = False
+        self._playing = False
+
+    def number_of_videos(self):
+        num_videos = len(self._video_library.get_all_videos())
+        print(f"{num_videos} videos in the library")
+
+    def show_all_videos(self):
+        """Returns all videos."""
+
+        print("Here's a list of all available videos:")
+        video_list = self._video_library.get_all_videos()
+        video_list.sort(key=lambda x: x.title)
+        for video in video_list:
+            format = f"{video.title} ({video.video_id}) [{' '.join(video.tags)}]"
+            print(f'\t {format}')
+
+    def play_video(self, video_id):
+        """Plays the respective video.
+
+        Args:
+            video_id: The video_id to be played.
+        """
+        video = self._video_library.get_video(video_id)
+
+        if not video:
+            print("Cannot play video: Video does not exist")
+            return
+
+        if self._current_video != None:
+            print(f"Stopping video: {self._current_video.title}")
+            print(f"Playing video: {video.title}")
+            self._current_video = video
+            self._playing = True
+            return
+
+        print(f"Playing video: {video.title}")
+        self._current_video = video
+        self._playing = True
+
+    def stop_video(self):
+        """Stops the current video."""
+
+        video = self._current_video
+        if video:
+            print(f'Stopping video: {self._current_video.title}')
+            self._current_video = None
+            self._playing = False
+            self._pause_video = True
+
+        else:
+            print("Cannot stop video: No video is currently playing")
+
+    def play_random_video(self):
+        """Plays a random video from the video library."""
+
+        if self._current_video != None:
+            print(f"Stopping video: {self._current_video.title}")
+
+            all_video = self._video_library.get_all_videos()
+            random_video = random.choice(all_video)
+            print(f"Playing video: {random_video.title}")
+
+            self._current_video = random_video
+            self._playing = True
+            return
+
+        if self._current_video == None:
+            all_video = self._video_library.get_all_videos()
+            random_video = random.choice(all_video)
+            print(f"Playing video: {random_video.title}")
+
+    def pause_video(self):
+        """Pauses the current video."""
+        video = self._current_video
+        if self._pause_video == False and self._current_video != None:
+            print(f'Pausing video: {self._current_video.title}')
+            self._pause_video = True
+            self._playing = False
+            return
+
+        if self._pause_video == True and self._current_video != None:
+            print(f"Video already paused: {video.title}")
+
+
+        else:
+            print("Cannot pause video: No video is currently playing")
+
+    def continue_video(self):
+        """Resumes playing the current video."""
+
+        video = self._current_video
+        if self._pause_video == True and self._current_video != None:
+            print(f'Continuing video: {video.title}')
+            self._pause_video = False
+            self.playing = True
+            return
+
+        if self._pause_video == False and self._current_video != None:
+            print("Cannot continue video: Video is not paused")
+            return
+
+        if self._playing == False and self._current_video == None:
+            print("Cannot continue video: No video is currently playing")
+
+    def show_playing(self):
+        """Displays video currently playing."""
+
+        video = self._current_video
+
+        if self._current_video == None and self._playing == False:
+            print("No video is currently playing")
+            return
+
+        if self._current_video != None and self._playing == False:
+            format = f"{video.title} ({video.video_id}) [{' '.join(video.tags)}]"
+            print(f'Currently playing: {format} - PAUSED')
+            return
+        if self._current_video != None and self._playing == True:
+            format = f"{video.title} ({video.video_id}) [{' '.join(video.tags)}]"
+            print(f'Currently playing: {format}')
+
+
+    def create_playlist(self, playlist_name):
+        """Creates a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print(f'Successfully created new playlist: {playlist_name}')
+
+    def add_to_playlist(self, playlist_name, video_id):
+        """Adds a video to a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+            video_id: The video_id to be added.
+        """
+        print("add_to_playlist needs implementation")
+
+    def show_all_playlists(self):
+        """Display all playlists."""
+
+        print("show_all_playlists needs implementation")
+
+    def show_playlist(self, playlist_name):
+        """Display all videos in a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print("show_playlist needs implementation")
+
+    def remove_from_playlist(self, playlist_name, video_id):
+        """Removes a video to a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+            video_id: The video_id to be removed.
+        """
+        print("remove_from_playlist needs implementation")
+
+    def clear_playlist(self, playlist_name):
+        """Removes all videos from a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print("clears_playlist needs implementation")
+
+    def delete_playlist(self, playlist_name):
+        """Deletes a playlist with a given name.
+
+        Args:
+            playlist_name: The playlist name.
+        """
+        print("deletes_playlist needs implementation")
+
+    def search_videos(self, search_term):
+        """Display all the videos whose titles contain the search_term.
+
+        Args:
+            search_term: The query to be used in search.
+        """
+        print("search_videos needs implementation")
+
+    def search_videos_tag(self, video_tag):
+        """Display all videos whose tags contains the provided tag.
+
+        Args:
+            video_tag: The video tag to be used in search.
+        """
+        print("search_videos_tag needs implementation")
+
+    def flag_video(self, video_id, flag_reason=""):
+        """Mark a video as flagged.
+
+        Args:
+            video_id: The video_id to be flagged.
+            flag_reason: Reason for flagging the video.
+        """
+        print("flag_video needs implementation")
+
+    def allow_video(self, video_id):
+        """Removes a flag from a video.
+
+        Args:
+            video_id: The video_id to be allowed again.
+        """
+        print("allow_video needs implementation")
